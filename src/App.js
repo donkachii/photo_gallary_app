ebd9489fbfa2551568bfbb7e188669a6805d75e3
Onyekachi Nwakaihe <onyekachi.nwakaihe@africaprudential.com>
2023-06-21 13:02:53 +0100

    Added Search Functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SearchAppBar from "./components/AppBar";
 import Photo from "./pages/Photo";
 
 function App() {
+  const [filter, setFilter] = useState("");
   return (
     <BrowserRouter>
-      <SearchAppBar />
+      <SearchAppBar setFilter={setFilter} filter={filter} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Photo />} />
+        <Route
+          path="/"
+          element={<Home setFilter={setFilter} filter={filter} />}
+        />
+        <Route
+          path="/:id"
+          element={<Photo setFilter={setFilter} filter={filter} />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,11 @@ import Grid from "@mui/material/Grid";
 import photos from "../data.json";
 import { Typography } from "@mui/material";
 
-const Home = () => {
+const Home = ({ filter }) => {
+  const filteredPhotos = photos.images.filter((item) =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <Container sx={{ marginY: 5 }}>
@@ -18,9 +22,22 @@ const Home = () => {
           spacing={{ xs: 1, sm: 3, md: 5 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          {photos.images.map((photo, i) => (
-            <ImageCard photo={photo} id={photo.id} key={i} />
-          ))}
+          {filteredPhotos.length > 0 ? (
+            filteredPhotos.map((photo, i) => (
+              <ImageCard photo={photo} id={photo.id} key={i} />
+            ))
+          ) : (
+            <Grid item>
+              <Typography
+                variant="paragraph"
+                component="p"
+                marginTop={3}
+                marginBottom={3}
+              >
+                No pictures found from the search
+              </Typography>
+            </Grid>
+          )}
         </Grid>
       </Container>
     </div>
